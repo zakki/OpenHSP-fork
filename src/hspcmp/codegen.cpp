6f91615aabbd9782f5bb02e8a579128386df84e6
Kensuke Matsuzaki <knsk.mtzk@gmail.com>
2026-03-16 19:14:19 +0900

    Revert "HSP3_TYPE_USERは内部コマンドでも使うので、プラグイン用のHSP3_TYPE_PLUGINを分離"
    This reverts commit 76c247bedd1aabb47e30243afccf9c2389930947.

--- src/hspcmp/codegen.cpp
+++ src/hspcmp/codegen.cpp
@@ -1451,6 +1451,7 @@ void CToken::GenerateCodePP_regcmd( void )
 	//
 	char cmd[1024];
 	char cmd2[1024];
+	cg_pptype = cg_typecnt;
 	cmd[0] = 0;
 
 	GetTokenCG( GETTOKEN_DEFAULT );
@@ -1473,11 +1474,10 @@ void CToken::GenerateCodePP_regcmd( void )
 		}
 
 		PutHPI( HPIDAT_FLAG_TYPEFUNC, 0, cmd2, cmd );
-		cg_pptype = cg_typecnt;
 		cg_typecnt++;
 		break;
 	case TK_NUM:
-		// PutHPI( HPIDAT_FLAG_SELFFUNC, 0, "", "" );
+		PutHPI( HPIDAT_FLAG_SELFFUNC, 0, "", "" );
 		cg_pptype = val;
 		break;
 	case TK_NONE:
@@ -2407,7 +2407,7 @@ void CToken::ResetGenerator(unsigned char *ptr)
 	line = 0;
 	cg_flag = CG_FLAG_ENABLE;
 	cg_valcnt = 0;
-	cg_typecnt = HSP3_TYPE_PLUGIN;
+	cg_typecnt = HSP3_TYPE_USER;
 	cg_pptype = -1;
 	cg_iflev = 0;
 	cg_wp = ptr;
